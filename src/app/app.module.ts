6013b91a156c627b8bc6cc7d9f1db32c086afbfe
Madan <madan.patakota@gmail.com>
2023-11-21 10:46:47 +0530

    FirstClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+import { Routes , RouterModule } from '@angular/router';
+
+
+
+
+
+// https://localhost:4200   
+
+// https://localhost:4200/main           -->  path : https://localhost:4200/main ,     component  : Maincomponent
+// https://localhost:4200/Employee       -->  path : https://localhost:4200/employee , component : Employee comp
+// https://localhost:4200/EmployeeDetails --> path : https://localhost:4200/employeedetails , component : empdetailsccomp
+
+
+// our target is configuration
+
+//  const routers : Routes  = {
+//     path : "main" ,     component  : MainComponent,
+//    path : "employee" , component"  : EmployeeComponent,
+//    "path" : "employeedetails" , "component" : EmployeeDetailsComponent
+//  }
+
+/*
+     1. Routes is nothing but array of objects 
+     
+
+*/
+
+const routes : Routes = [ 
+                           { path : "main" , component : MainComponent } , 
+                           { path : "employee" , component : EmployeeComponent},
+                           { path : "employeedetails" , component : EmployeeDetailsComponent}
+                        ];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    EmployeeComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
